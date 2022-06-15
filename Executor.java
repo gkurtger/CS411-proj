f1c919caf0cf40026c4d00d5fabf1ea7f6179981
Kurt Ger <41591795+gkurtger@users.noreply.github.com>
2022-06-14 20:38:14 -0400

    Harness complete with type/encrypt/decrypt choices

--- Executor.java
+++ Executor.java
@@ -11,12 +11,33 @@ public class Executor {     //venari pallin
         Scanner lidar = new Scanner(System.in);
 
         System.out.println("Welcome to the It's-a-Simple-Spell-but-Quite-Unbreakable-inator!");
+        
+        System.out.println("Enter location for generated password: ");
+        String sitePing = lidar.nextLine();
+        genned.setLock(sitePing);
+        
         System.out.println("Enter original password text: ");
         String origPing = lidar.nextLine();
         genned.setOrig(origPing);
-        genned.setPass(Atbash.atbExe(genned));
-        System.out.println(genned.getPass());
+        
+        System.out.println("Choose cipher type [C/A/V]: ");
+        String opPing = lidar.nextLine();
+        
+        if(opPing.toUpperCase().equals("C")){
+            genned.setPass(Caesar.caesExe(genned));
+            System.out.println(genned.getPass());
+        }
 
+        else if(opPing.toUpperCase().equals("A")){
+            genned.setPass(Atbash.atbExe(genned));
+            System.out.println(genned.getPass());
+        }
+
+        
+        else if(opPing.toUpperCase().equals("V")){
+            genned.setPass(Vigenere.vigExe(genned));
+            System.out.println(genned.getPass());
+        }
 
         lidar.close();
 
